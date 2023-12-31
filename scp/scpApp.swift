021a5bbbb345b740e0b0cdf638732fcf6d46d77f
dzytko <dawid.zytko@gmail.com>
2023-12-31 22:55:48 +0100

    Initial Commit

---
+++ scp/scpApp.swift
@@ -0,0 +1,17 @@
+//
+//  scpApp.swift
+//  scp
+//
+//  Created by a on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct scpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
